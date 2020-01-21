ccc0d8cd3cb8110deafa1b8e059000d4633e74cb
ljh-c <57099181+ljh-c@users.noreply.github.com>
2020-01-20 23:32:09 -0800

    initial state

---
+++ src/reducers/index.js
@@ -0,0 +1,42 @@
+export const initialState = [
+  {
+    task: 'Buy it',
+    completed: false,
+    id: 0,
+  },
+  {
+    task: 'Use it',
+    completed: false,
+    id: 1
+  },
+  {
+    task: 'Break it',
+    completed: false,
+    id: 2
+  },
+  {
+    task: 'Fix it',
+    completed: false,
+    id: 3
+  },
+  {
+    task: 'Trash it',
+    completed: false,
+    id: 4
+  },
+  {
+    task: 'Change it',
+    completed: false,
+    id: 5
+  },
+  {
+    task: 'Mail',
+    completed: false,
+    id: 6
+  },
+  {
+    task: 'Upgrade it',
+    completed: false,
+    id: 7
+  }
+];
